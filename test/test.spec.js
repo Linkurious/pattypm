fcf9048f9e481fdf48601dfcf5519508c9b976a4
davidrapin <david@linkurio.us>
2021-02-15 17:28:23 +0100

    #14 making tests more robust by using absolute paths for test output files

--- test/test.spec.js
+++ test/test.spec.js
@@ -15,6 +15,8 @@ describe('Full stack', function() {
   it('should pass client env vars to services when cleanEnv=undefined', function(done) {
     this.timeout(5 * 1000);
 
+    const outFile = path.resolve(__dirname, 'out-t1.txt');
+
     // make config file
     const configPath = TestUtils.makeConfig({
       port: 4301,
@@ -26,30 +28,29 @@ describe('Full stack', function() {
         {
           name: 'Service ONE',
           binPath: '/bin/bash',
-          arguments: ['-c', 'echo lol1:$LOL1 lol2:$LOL2 > s1.txt'],
+          arguments: ['-c', 'echo lol1:$LOL1 lol2:$LOL2 > ' + outFile],
           env: {LOL2: 'lol2-value'}
         }
       ]
     });
 
     // start PPM
-    const cp = TestUtils.runClient(configPath, 'start', {LOL1: 'haha'});
+    TestUtils.runClient(configPath, 'start', {LOL1: 'haha'});
 
     // stop PPM after 2 seconds
     const stop = () => {
-      const cp2 = TestUtils.runClient(configPath, 'stop');
-      cp2.on('exit', () => after());
+      const cp = TestUtils.runClient(configPath, 'stop');
+      cp.on('exit', () => after());
     };
     setTimeout(stop, 2500);
 
     // after PPM has stopped
     const after = () => {
-      const s1 = TestUtils.readDelete(path.resolve(__dirname, 's1.txt'));
-
+      const output = TestUtils.readDelete(outFile);
       TestUtils.cleanUp(configPath);
 
       // without cleanEnv (LOL1 is passed)
-      should(s1).equal('lol1:haha lol2:lol2-value\n');
+      should(output).equal('lol1:haha lol2:lol2-value\n');
 
       done();
     };
@@ -57,6 +58,8 @@ describe('Full stack', function() {
 
   it('should *not* pass client env vars to services when cleanEnv=true', function(done) {
     this.timeout(5 * 1000);
+
+    const outFile = path.resolve(__dirname, 'out-t2.txt');
 
     // make config file
     const configPath = TestUtils.makeConfig({
@@ -70,30 +73,29 @@ describe('Full stack', function() {
           name: 'Service ONE',
           binPath: '/bin/bash',
           cleanEnv: true,
-          arguments: ['-c', 'echo lol1:$LOL1 lol2:$LOL2 > s2.txt'],
+          arguments: ['-c', 'echo lol1:$LOL1 lol2:$LOL2 > ' + outFile],
           env: {LOL2: 'lol2-value'}
         }
       ]
     });
 
     // start PPM
-    const cp = TestUtils.runClient(configPath, 'start', {LOL1: 'haha'});
+    TestUtils.runClient(configPath, 'start', {LOL1: 'haha'});
 
     // stop PPM after 2 seconds
     const stop = () => {
-      const cp2 = TestUtils.runClient(configPath, 'stop');
-      cp2.on('exit', () => after());
+      const cp = TestUtils.runClient(configPath, 'stop');
+      cp.on('exit', () => after());
     };
     setTimeout(stop, 2500);
 
     // after PPM has stopped
     const after = () => {
-      const s2 = TestUtils.readDelete(path.resolve(__dirname, 's2.txt'));
-
+      const output = TestUtils.readDelete(outFile);
       TestUtils.cleanUp(configPath);
 
       // with cleanEnv:true (LOL1 is empty)
-      should(s2).equal('lol1: lol2:lol2-value\n');
+      should(output).equal('lol1: lol2:lol2-value\n');
 
       done();
     };
